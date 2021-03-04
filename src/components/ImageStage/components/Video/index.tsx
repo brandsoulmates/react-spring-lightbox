965047c742ec0718a1875500a20cc201dc2f7034
Howard Long <long.yhoward@gmail.com>
2021-03-03 16:43:04 -0800

    removed unused

--- src/components/ImageStage/components/Video/index.tsx
+++ src/components/ImageStage/components/Video/index.tsx
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { useDoubleClick } from '../../utils';
 
 type VideoProps = {
     component: React.Component;
@@ -9,8 +10,8 @@ type VideoProps = {
  * Animates pinch-zoom + panning on image using spring physics
  */
 const Video = ({ component, pagerIsDragging }: VideoProps) => {
-    const isDragging = useRef<boolean>(false);
-    // const videoRef = useRef<HTMLInputElement>(null);
+    // const isDragging = useRef<boolean>(false);
+    const videoRef = useRef<HTMLInputElement>(null);
     /**
      * Animates scale and translate offsets of Image as they change in gestures
      *
@@ -37,52 +38,47 @@ const Video = ({ component, pagerIsDragging }: VideoProps) => {
     //     },
     // });
 
-    // useDoubleClick({
-    //     ['onSingleClick']: (e: MouseEvent) => {
-    //         if (pagerIsDragging) {
-    //             console.log("IT'S CLICKING");
-    //             e.stopPropagation();
-    //             e.preventDefault();
-    //             e.stopImmediatePropagation();
-    //             e.nativeEvent.stopImmediatePropagation();
-    //             return;
-    //         }
-    //     },
-    //     latency: 0,
-    //     ref: videoRef,
-    // });
+    useDoubleClick({
+        capture: true,
+        latency: 0,
+        ['onSingleClick']: (e: MouseEvent) => {
+            if (pagerIsDragging) {
+                console.log("IT'S CLICKING");
+                e.stopPropagation();
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                return;
+            }
+        },
+
+        ref: videoRef,
+    });
 
     return (
         <div
-            onClick={() => console.log('NORMAL ONCLICK')}
-            onClickCapture={(e) => {
-                console.log('video click capture preventing default');
-                e.stopPropagation();
-                e.preventDefault();
-                e.nativeEvent.stopImmediatePropagation();
-                if (isDragging.current) {
-                    console.log('video click capture is dragging');
-                    e.stopPropagation();
-                    e.preventDefault();
-                    e.nativeEvent.stopImmediatePropagation();
-                }
-            }}
-            onMouseDownCapture={(e) => {
-                isDragging.current = true;
-                console.log('video mouse down capture');
-                e.preventDefault();
-                e.stopPropagation();
-                e.nativeEvent.stopImmediatePropagation();
-            }}
-            onMouseUpCapture={(e) => {
-                console.log('video mouse up capture');
-                isDragging.current = pagerIsDragging;
-                e.preventDefault();
-                e.stopPropagation();
-                e.nativeEvent.stopImmediatePropagation();
-            }}
-
-            // ref={videoRef}
+            // onClickCapture={(e) => {
+            //     console.log('video click capture preventing default');
+            //     e.stopPropagation();
+            //     e.preventDefault();
+            //     if (isDragging.current) {
+            //         console.log('video click capture is dragging');
+            //         e.stopPropagation();
+            //         e.preventDefault();
+            //     }
+            // }}
+            // onMouseDownCapture={(e) => {
+            //     isDragging.current = true;
+            //     console.log('video mouse down capture');
+            //     e.preventDefault();
+            //     e.stopPropagation();
+            // }}
+            // onMouseUpCapture={(e) => {
+            //     console.log('video mouse up capture');
+            //     isDragging.current = pagerIsDragging;
+            //     e.preventDefault();
+            //     e.stopPropagation();
+            // }}
+            ref={videoRef}
         >
             {component}
         </div>
--- src/components/ImageStage/utils/useDoubleClick.tsx
+++ src/components/ImageStage/utils/useDoubleClick.tsx
@@ -1,12 +1,15 @@
 import React, { useEffect } from 'react';
 
 type IUseDoubleClickProps = {
+    capture?: boolean;
     /** The amount of time (in milliseconds) to wait before differentiating a single from a double click */
     latency?: number;
     /** A callback function for double click events */
     onDoubleClick?: (event: MouseEvent) => void;
+
     /** A callback function for single click events */
     onSingleClick?: (event: MouseEvent) => void;
+
     /** Dom node to watch for double clicks */
     ref: React.RefObject<HTMLElement>;
 };
@@ -17,6 +20,7 @@ type IUseDoubleClickProps = {
  */
 const useDoubleClick = ({
     ref,
+    capture,
     latency = 300,
     onSingleClick = () => null,
     onDoubleClick = () => null,
@@ -38,11 +42,11 @@ const useDoubleClick = ({
         };
 
         // Add event listener for click events
-        clickRef?.addEventListener('click', handleClick);
+        clickRef?.addEventListener('click', handleClick, capture);
 
         // Remove event listener
         return () => {
-            clickRef?.removeEventListener('click', handleClick);
+            clickRef?.removeEventListener('click', handleClick, capture);
 
             if (timer) {
                 clearTimeout(timer);
